e1d0f89dc9d2fe89cf871c7393555713c2d9f2ba
Adriaan Beiertz <adriaanbd@gmail.com>
2020-10-27 15:02:54 -0500

    added private subnet with and instance

--- kata-1/main.tf
+++ kata-1/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "pub_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.pub_sub_cidr
   availability_zone       = var.az
@@ -45,7 +45,7 @@ resource "aws_route_table" "rt" {
 }
 
 resource "aws_route_table_association" "rt-assoc" {
-  subnet_id      = aws_subnet.main.id
+  subnet_id      = aws_subnet.pub_a.id
   route_table_id = aws_route_table.rt.id
 }
 
@@ -83,7 +83,7 @@ resource "aws_instance" "bastion_host" {
   ami                    = data.aws_ssm_parameter.ami.value
   instance_type          = var.instance_type
   key_name               = var.key_name
-  subnet_id              = aws_subnet.main.id
+  subnet_id              = aws_subnet.pub_a.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   tags = {
     Name        = "${var.namespace}-ec2"
@@ -91,4 +91,89 @@ resource "aws_instance" "bastion_host" {
   }
 }
 
+resource "aws_eip" "nat_eip" {
+  vpc         = true
+  depends_on  = [aws_internet_gateway.igw]
+  tags = {
+    Name      = "${var.namespace}-nat_eip"
+  }
+}
 
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.pub_a.id
+  tags = {
+    Name        = "${var.namespace}-ngw"
+    Env         = "dev"
+  }
+}
+
+resource "aws_subnet" "app_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.priv_sub_cidr
+  availability_zone       = var.az
+  # if true, instances will be accessible via internet gateway
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name                  = "${var.namespace}-subnet-app_a"
+    Environment           = "dev"
+  }
+}
+
+resource "aws_route_table" "app_a-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name        = "${var.namespace}-app_a-rt"
+    Environment = "dev"
+  }
+}
+
+resource "aws_route_table_association" "app_a-rt_assoc" {
+  subnet_id      = aws_subnet.pub_a.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "app_a-sg" {
+  name          = "app_a-sg"
+  description   = "Allow SSH"
+  vpc_id        = aws_vpc.main.id  # if not provided defaults to default VPC
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    # https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    from_port   = 8  # from icmp parameter type "Echo"
+    to_port     = 0  # from icmp parameter type "Echo Reply"
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.namespace}-app_a-sg"
+    Environemnt = "dev"
+  }
+}
+
+resource "aws_instance" "app_host" {
+  ami                    = data.aws_ssm_parameter.ami.value
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.pub_a.id
+  vpc_security_group_ids = [aws_security_group.app_a-sg.id]
+  tags = {
+    Name                 = "${var.namespace}-ec2-app_host"
+    Env                  = "dev"
+  }
+}
