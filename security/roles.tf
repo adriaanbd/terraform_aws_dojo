20f71ea18f4c65d84b82c7bdea0f7ae8fa6fc766
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-01 22:10:03 -0500

    added variables and outputs to iam roles

--- security/roles.tf
+++ security/roles.tf
@@ -10,6 +10,7 @@ data "aws_iam_policy_document" "ecs_policy" {
   }
 }
 
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/instance_IAM_role.html
 data "aws_iam_policy_document" "ecs_instance_policy" {
   version = "2012-10-17"
   statement {
@@ -22,30 +23,29 @@ data "aws_iam_policy_document" "ecs_instance_policy" {
 }
 
 resource "aws_iam_role" "ecs_role" {
-  name                = "ecs-service-role"
+  name                = var.ecs_service_role_name
   path                = "/"
   assume_role_policy  = data.aws_iam_policy_document.ecs_policy.json
 }
 
 resource "aws_iam_role" "ecs_instance_role" {
-    name                = "ecs-instance-role"
+    name                = var.ecs_instance_role_name
     path                = "/"
     assume_role_policy  = data.aws_iam_policy_document.ecs_instance_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_role_attchmnt" {
   role       = aws_iam_role.ecs_role.name
-  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/instance_IAM_role.html
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+  policy_arn = var.arn_iam_ec2_ecs_service_policy
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_instance_role_attchmnt" {
   role       = aws_iam_role.ecs_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+  policy_arn = var.arn_iam_ec2_ecs_service_policy
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_instance_profile
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "ecs-instance_profile"
+  name = var.ecs_instance_profile_name
   role = aws_iam_role.ecs_instance_role.name
 }
--- security/variables.tf
+++ security/variables.tf
@@ -4,6 +4,32 @@ variable "namespace" {
 }
 
 variable "vpc_id" {
-    type = string
+    type        = string
     description = "ID of the Main VPC"
+}
+
+variable "ecs_service_role_name" {
+  type          = string
+  description   = "Given name of the ECS service role"
+  default       = "ecs-service-role"
+}
+
+variable "ecs_instance_role_name" {
+  type          = string
+  description   = "Given name of the ECS instance role"
+  default       = "ecs-instance-role"
+}
+
+variable "ecs_instance_profile_name" {
+  type          = string
+  description   = "Given name of the ECS instance profile"
+  default       = "ecs-instance_profile"
+}
+
+
+variable "arn_iam_ec2_ecs_service_policy" {
+  type          = string
+  description   = "AWS ARN for the ECS "
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/instance_IAM_role.html
+  default       = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
