93e1470ef7aaf82838f0eb40744dbf50b32f2147
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-02 17:25:40 -0500

    corrected main vars for pub subs, app sg and ecs

--- main.tf
+++ main.tf
@@ -14,17 +14,17 @@ module "compute" {
   namespace             = var.namespace
   key_pair_name         = var.key_pair_name
   pub_a_id              = module.network.pub_a_id
-  pub_b_id              = module.network.pub_a_id
-  pub_c_id              = module.network.pub_a_id
+  pub_b_id              = module.network.pub_b_id
+  pub_c_id              = module.network.pub_c_id
   priv_a_id             = module.network.priv_a_id
   priv_b_id             = module.network.priv_b_id
   priv_c_id             = module.network.priv_c_id
   bastion_sg_a          = module.security.bastion_sg_a
   bastion_sg_b          = module.security.bastion_sg_a
   bastion_sg_c          = module.security.bastion_sg_a
-  app_sg_a              = module.security.app_sg_a
-  app_sg_b              = module.security.app_sg_b
-  app_sg_c              = module.security.app_sg_c
+  app_sg_a_id           = module.security.app_sg_a
+  app_sg_b_id           = module.security.app_sg_b
+  app_sg_c_id           = module.security.app_sg_c
   ecs_profile_name      = module.security.ecs_instance_profile_name
-  ecs_service_role_name = module.security.ecs_service_role_name
+  # ecs_service_role_name = module.security.ecs_service_role_name
 }
